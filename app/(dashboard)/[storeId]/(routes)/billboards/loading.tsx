819ae75fcc555d50d9fd2d3df3d8aabc8b23259e
torvic <torvic02st96@gmail.com>
2024-01-04 20:41:03 -0500

    ADD billboard enity and create/edit billboard page

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/loading.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+// import { Loader } from "@/components/ui/loader";
+
+const Loading = () => {
+  return ( 
+    <div className="flex h-full w-full items-center justify-center">
+    </div>
+   );
+}
+ 
+export default Loading;
